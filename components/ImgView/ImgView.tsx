2022ccc7da1b665404b560a7bcc97927a79bdb65
vicarb <vcardenasb@protonmail.com>
2023-06-15 01:39:39 -0400

    added react spring functionalities

--- components/ImgView/ImgView.tsx
+++ components/ImgView/ImgView.tsx
@@ -1,6 +1,32 @@
 'use client'
 import Image from 'next/image';
 import { useEffect, useState } from 'react';
+import { animated, useSpring } from '@react-spring/web';
+
+const ImageCard = ({ src, alt }) => {
+  const [springProps, setSpringProps] = useSpring(() => ({
+    scale: 1,
+    config: { mass: 5, tension: 350, friction: 40 },
+  }));
+
+  return (
+    <animated.div 
+      className="relative h-[33vh] md:h-[50vh] lg:h-[33vh] rounded-lg overflow-hidden" 
+      onMouseEnter={() => setSpringProps({ scale: 1.05 })} 
+      onMouseLeave={() => setSpringProps({ scale: 1 })}
+      style={{
+        transform: springProps.scale.to(scale => `scale(${scale})`),
+      }}
+    >
+      <Image
+        src={src}
+        alt={alt}
+        layout="fill"
+        objectFit="cover"
+      />
+    </animated.div>
+  );
+}
 
 export default function ImgView() {
   const [products, setProducts] = useState([]);
@@ -12,26 +38,12 @@ export default function ImgView() {
   }, []);
 
   return (
-    <div className="grid grid-cols-4 gap-4">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
       {products.map(product => (
         <>
-          <div key={product.id} className="relative h-[33vh] rounded-lg overflow-hidden hover:shadow-lg transition-shadow duration-300 ease-in-out transform hover:scale-105">
-            <Image
-              src={product.mainImage}
-              alt={product.name}
-              layout="fill"
-              objectFit="cover"
-            />
-          </div>
+          <ImageCard key={product.id} src={product.mainImage} alt={product.name} />
           {product.extraImages.map((image, index) => (
-            <div key={index} className="relative h-[33vh] rounded-lg overflow-hidden hover:shadow-lg transition-shadow duration-300 ease-in-out transform hover:scale-105">
-              <Image
-                src={image}
-                alt={`${product.name} extra image`}
-                layout="fill"
-                objectFit="cover"
-              />
-            </div>
+            <ImageCard key={index} src={image} alt={`${product.name} extra image`} />
           ))}
         </>
       ))}
