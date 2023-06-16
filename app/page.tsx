56d957fc6d36e3be1fe783a36a39140dd2d7ffdd
vicarb <vcardenasb@protonmail.com>
2023-06-16 13:23:39 -0400

    updated margin

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import Image from 'next/image'
 import ImgView from '@/components/ImgView/ImgView'
 export default function Home() {
   return (
+    <div className='mt-4'>
     <ImgView/>
+    </div>
   )
 }
